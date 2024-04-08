be000bf7e461bf8e56bcbf00a8994c61508bddfb
hamzahussain15 <163432696+hamzahussain15@users.noreply.github.com>
2024-04-08 14:20:43 +0500

    Pushed my standard simple calculator to github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Standard-Simple-Calculator" 
---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "Kindly select the first number", type: "number", name: "firstnumber" },
+    { message: "Kindly select the second number", type: "number", name: "secondnumber" },
+    {
+        message: " Kindly select one of the operators to perform the desired action",
+        type: "list",
+        name: "useroperator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division", "Percentage"],
+    },
+]);
+if (asnwer.useroperator === "Addition") {
+    console.log(asnwer.firstnumber + asnwer.secondnumber);
+}
+else if (asnwer.useroperator === "Subtraction") {
+    console.log(asnwer.firstnumber - asnwer.secondnumber);
+}
+else if (asnwer.useroperator === "Multiplication") {
+    console.log(asnwer.firstnumber * asnwer.secondnumber);
+}
+else if (asnwer.useroperator === "Division") {
+    console.log(asnwer.firstnumber / asnwer.secondnumber);
+}
+else if (asnwer.useroperator === "Percentage") {
+    console.log(asnwer.firstnumber % asnwer.secondnumber);
+}
+console.log("Thankyou for using");
+console.log("Hope You Liked our application");
